5ec88eb493736d0287a78bc02a2a1f58c37a9987
andronovman <andronovman@mail.ru>
2023-04-04 19:24:12 +0400

    define project

---
+++ src/main/java/ru/andronovman/onlycore/OnlycoreApplication.java
@@ -0,0 +1,13 @@
+package ru.andronovman.onlycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlycoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlycoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andronovman/onlycore/OnlycoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andronovman.onlycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlycoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
